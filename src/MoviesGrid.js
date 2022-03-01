6cf4c8594cc6076665eab98ed9098dcc67947b3d
AlexJob <henaochacon@gmail.com>
2022-03-01 15:31:07 -0500

    First commit

---
+++ src/MoviesGrid.js
@@ -0,0 +1,10 @@
+import MovieCard from './MovieCard';
+import movies from './movies.json';
+import styles from './MovieGrid.module.css';
+export default function MoviesGrid() {
+    return (
+        <ul className={styles.moviesGrid}>
+            {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
+        </ul>
+    );
+}
