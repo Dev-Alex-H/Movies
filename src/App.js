cf2c3e53b9ca63c429e8b331595cf6a6a4f652bf
AlexJob <henaochacon@gmail.com>
2022-03-01 22:20:51 -0500

    React Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import MoviesGrid from "./MoviesGrid";
+import MoviesGrid from "./components/MoviesGrid";
 import styles from './App.module.css';
+
+import {
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
 
 export default function App() {
     return (
         <div>
             <header>
                 <h1 className={styles.title}>Movies</h1>
+                <Link to="/">Home</Link>
+                <Link to="/ejemplox">Movie</Link>
             </header>
             <main>
-                <MoviesGrid />
+                <Routes>
+                    <Route path="/" element={<MoviesGrid />} />
+                </Routes>
             </main>
         </div>
     );
