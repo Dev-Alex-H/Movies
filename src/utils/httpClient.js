6af54e9bf265b60bb8500d9f7b6819b6acbb3a93
AlexJob <henaochacon@gmail.com>
2022-03-04 23:36:02 -0500

    Desplegar app con netlify

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,5 +1,5 @@
-const API = "https://api.themoviedb.org/3";
-const api_key = "api_key=79272c5618c09e303d8611a7339532ca";
+const API = process.env.REACT_APP_API;
+const api_key = process.env.REACT_APP_API_KEY;
 export default function get(path, page = "") {
     return fetch(API + path + api_key + page)
         .then(result => result.json());
