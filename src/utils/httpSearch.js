fa4395400eb0ee39b6233bf932422920582a6e7e
AlexJob <henaochacon@gmail.com>
2022-03-05 00:09:40 -0500

    Arreglo

--- src/utils/httpSearch.js
+++ src/utils/httpSearch.js
@@ -1,7 +1,7 @@
-const API = process.env.REACT_APP_API;
-const api_key = process.env.REACT_APP_API_KEY;
+const API = "https://api.themoviedb.org/3/search/movie?";
+const api_key = "api_key=79272c5618c09e303d8611a7339532ca";
 
-export function getSearch(path, page) {
+export function getSearch(path, page = "") {
     return fetch(`${API}${api_key}&query=${path}&page=${page}`)
         .then(result => result.json());
 }
